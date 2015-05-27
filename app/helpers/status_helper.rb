96a026c34d03897ff3e0117449b742d961af5fe9
Gabriel-Inacio <Gabrielinac@gmail.com>
2015-05-27 16:42:06 -0300

    status

---
+++ app/helpers/status_helper.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+module StatusHelper
+
+  SITUATION = {
+      0 => "A receber",
+      1 => "Recebido"
+  }
+
+  PAID_DRIVER = {
+      0 => "Não",
+      1 => "sim"
+  }
+end
--- app/models/freight.rb
+++ app/models/freight.rb
@@ -6,7 +6,7 @@ class Freight < ActiveRecord::Base
 
   attr_accessible :truckId, :truck, :motorist, :motoristId, :client, :clientId, :origin,
       :destination, :valueKm, :distanceKm, :description, :situation, :spent, :descriptionSpent,
-      :numberHead, :exitDate, :arrivalDate
+      :numberHead, :exitDate, :arrivalDate, :paidDriver
 
   #validates :cpf, cpf: true, :allow_blank => true
 
