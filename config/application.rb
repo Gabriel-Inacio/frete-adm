8982eaaff164144015ce27eb87e5d98ab4aa6498
Gabriel-Inacio <Gabrielinac@gmail.com>
2015-05-27 14:41:57 -0300

    set locale

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,13 @@ module FreteAdm
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.i18n.default_locale = :"pt-BR"
+
+    config.active_record.default_timezone = :local
+
+    config.encoding = "utf-8"
+
+    config.time_zone = 'Brasilia'
   end
 end
