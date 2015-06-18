b13ccfdcfb6091679e8570114ddca29a74f16625
Gabriel-Inacio <Gabrielinac@gmail.com>
2015-06-18 10:35:24 -0300

    mask numbers

--- app/assets/javascripts/lib/masks.js
+++ app/assets/javascripts/lib/masks.js
@@ -1,4 +1,32 @@
+// Only integer
+function isIntegerKey(event) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+// Only float
+function isFloatKey(sender, evt) {
+    var keyChar;
+    if (window.event)
+        keyChar = String.fromCharCode(window.event.keyCode);
+    else if (e)
+        keyChar = String.fromCharCode(e.which);
+    else
+        return true;
+    var val = (sender.value + keyChar).replace(/,/, '.');
+    var number = parseFloat(val);
+    if (number != val && val != '-')
+        return false;
+    return true;
+}
+
 $(window).load(function() {
     // CPF
-  $("input.cpf").mask("999.999.999-99");
+    $("input.cpf").mask("999.999.999-99");
+
+   //Dinheiro
+   $("input.money").maskMoney();
+
 });
